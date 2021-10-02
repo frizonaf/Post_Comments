cff65899ed7861d2117225434a9ae5bc61d0a2ed
Andre Frizon <andre.frizon@gmail.com>
2021-10-01 22:45:26 -0300

    Projeto pronto

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,52 +1,33 @@
 package application;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
-import java.util.Scanner;
 
 import entities.Comment;
 import entities.Post;
 
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		Date data = new Date();
 		
-		System.out.println("Quantas viagens você fez? ");
-		int number = sc.nextInt();
+		Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"),
+				"Traveling to New Zealand", 
+				"I'm going to visit this wonderful country", 12);
 		
-		for (int i = 0; i < number; i++) {
-			System.out.print("Viagem #" + (i + 1) + ": ");
-			String travel = sc.nextLine();
-			System.out.print("Quanto likes: ");
-			int likes = sc.nextInt();
-			System.out.println("Título da viagem: ");
-			String content = sc.nextLine();
-			
-			Post post = new Post(data, travel, content, likes);
-			
-			System.out.println();
-			System.out.print("Quantos comentários? ");
-			int numberComment = sc.nextInt();
-			
-			for (int j = 0; j < numberComment; j++) {
-				System.out.println();
-				System.out.print("Digite o commentário #" + (j + 1) + ": ");
-				String comment = sc.nextLine();
-				Comment c = new Comment(comment);
-				post.addComment(c);			
-			}
-			
-		}
+		Comment c1 = new Comment("Have a nice trip");
+		Comment c2 = new Comment("Wow that's awesome");
+
+		p1.addComment(c1);
+		p1.addComment(c2);
 		
-		for ()
+		System.out.println(p1);
 		
-		sc.close();
+		
+
 
 	}
 
--- src/entities/Post.java
+++ src/entities/Post.java
@@ -1,18 +1,22 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Post {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
 	private Date moment;
 	private String title;
 	private String content;
 	private Integer likes;
+	
 	private List<Comment> comments =  new ArrayList<>();
 	
 	public Post() {
-		
 	}
 	public Post(Date moment, String title, String content, Integer likes) {
 		this.moment = moment;
@@ -47,13 +51,26 @@ public class Post {
 	public List<Comment> getComments() {
 		return comments;
 	}
-	public void addComment(Comment comment) {
-		comments.add(comment);
+	public void addComment(Comment comment1) {
+		comments.add(comment1);
 	}
 	public void removeComment(Comment comment) {
 		comments.remove(comment);
 	}
 		
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(title + "\n");
+		sb.append(likes);
+		sb.append(" Likes - ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append(content + "\n");
+		sb.append("Comments: \n");
+		for (Comment c: comments) {
+			sb.append(c.getText() + "\n");
+		}
+		return sb.toString();
+	}
 	
 
 }
